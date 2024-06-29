b229ec4573fec18b7340141e27cb307e9dcf9e2b
Frederic Bicandy <fbicandy@c1r4s1.42beirut.com>
2024-06-29 18:41:07 +0300

    Upload

--- core/ft_printf/ft_printf.c
+++ core/ft_printf/ft_printf.c
@@ -6,38 +6,46 @@
 /*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:01:56 by fbicandy          #+#    #+#             */
-/*   Updated: 2024/06/26 22:43:08 by fbicandy         ###   ########.fr       */
+/*   Updated: 2024/06/29 18:39:19 by fbicandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <limits.h>
 #include "libft/libft.h"
+
+int	char_format(va_list args, const char format)
+{
+	int	count;
+
+	count = 0;
+	if (format == 'c')
+		count = ft_putchar_fd(va_arg(args, int), 1);
+	if (format == 's')
+		count = ft_putstr_fd(va_arg(args, char *), 1);
+	return (count);
+}
 
 int	nbr_format(va_list args, const char format)
 {
-	int		count;
-	int		num;
-	char	*str;
+	int	count;
 
+	count = 0;
 	if (format == 'd' || format == 'i')
-	{
-		num = va_arg(args, int);
-		str = ft_itoa(num);
-		ft_putstr_fd(str, 1);
-		count = ft_strlen(str);
-		free(str);
-	}
-	else if (format == 'u')
-	{
-		num = va_arg(args, unsigned int);
-		num *= -1;
-		str = ft_itoa(num);
-		ft_putstr_fd(str, 1);
-		count = ft_strlen(str);
-		free(str);
-	}
+		count = ft_putnbr_fd(va_arg(args, int), 1, 0);
+	if (format == 'u')
+		count = ft_u_putnbr_fd(va_arg(args, int), 1, 0);
 	return (count);
+}
+
+int	check_format(va_list args, const char format)
+{
+	if (format == 'd' || format == 'i' || format == 'u')
+		return (nbr_format(args, format));
+	if (format == 'c' || format == 's')
+		return (char_format(args, format));
+	return (0);
 }
 
 int	ft_printf(const char *PARAM, ...)
@@ -54,8 +62,7 @@ int	ft_printf(const char *PARAM, ...)
 		if (PARAM[i] == '%')
 		{
 			i++;
-			if (PARAM[i] == 'd' || PARAM[i] == 'i' || PARAM[i] == 'u')
-				count += nbr_format(args, PARAM[i]);
+			count += check_format(args, PARAM[i]);
 		}
 		else
 		{
@@ -70,12 +77,7 @@ int	ft_printf(const char *PARAM, ...)
 
 // int main(void)
 // {
-// 	int count;
-
-// 	printf("------------------------FT-----------------------------\n");
-// 	count = ft_printf("printf:test %u\n",-747);
-// 	printf("~%d\n", count);
-// 	printf("------------------------Original-----------------------------\n");
-// 	printf("~%d\n", printf("PRINTF:test %u\n",-123));
+// 	printf("\nCOUNT=%d\n",printf("he%c",'d'));
+// 	printf("\nCOUNT=%d\n",ft_printf("he%c",'d'));
 // 	return (0);
 // }
